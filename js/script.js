e50292f689529bf30af7f3ae4dcd40cbfbebb1b8
“jdelavega94” <jcdelavega94@gmail.com>
2023-03-28 14:38:12 +0200

    project completed

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const { createApp } = Vue;
+createApp({
+    data(){
+        return {
+            message: "ciao",
+            img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.jIqz0nwJ28om0AUV0E72uwHaKW%26pid%3DApi&f=1&ipt=5ab60061e12ead85b5d387dcbe7cf7376e81f3d368f9702487adbed8e992fd6b&ipo=images",
+            style: "width: 200px"
+        }
+    },
+    methods: {
+
+    }
+}).mount("#app")
